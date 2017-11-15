d71b7e97f2366ca9f05c843523da322fb48ac144
etudiant <etudiant@iMac-de-etudiant-28.local>
2017-11-15 14:51:28 +0100

    display map

--- MapKitTuto/MapViewController.swift
+++ MapKitTuto/MapViewController.swift
@@ -7,29 +7,30 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class MapViewController: UIViewController {
-
+    
+    @IBOutlet var mapView: MKMapView!
+    var locationManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        if CLLocationManager.locationServicesEnabled() {
+            let manager = CLLocationManager()
+            manager.requestWhenInUseAuthorization()
+            self.locationManager = manager
+        }
+        self.mapView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+extension MapViewController : MKMapViewDelegate {
 }
