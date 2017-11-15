13076688aa5ef2e8a02cc79b202a2242972f3d86
etudiant <etudiant@iMac-de-etudiant-28.local>
2017-11-15 14:56:22 +0100

    show location

--- MapKitTuto/MapViewController.swift
+++ MapKitTuto/MapViewController.swift
@@ -25,11 +25,6 @@ class MapViewController: UIViewController {
         self.mapView.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
 
 extension MapViewController : MKMapViewDelegate {
